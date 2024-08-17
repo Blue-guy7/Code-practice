3a309f82436ec11b26b58d3e30eaa732f305ef8b
unknown <nilangshuganguly71@gmail.com>
2024-08-17 21:51:14 +0530

    Finished program to sort each row in a 2d array

--- practice2.c
+++ practice2.c
@@ -3,7 +3,7 @@
 
 int main(){
     int arr[100][100];
-    int i,j,k,rows,cols;
+    int temp,i,j,k,rows,cols;
     printf("Enter the number of Rows:");
     scanf("%d",&rows);
     printf("Enter the number of columns:");
@@ -16,13 +16,24 @@ int main(){
     }
     /*Using bubble sort to sort the matrix */
     for (i=0;i<rows;i++){
-        for (j=0;j<cols;j++){
-            for (k=0;k<cols;k++){
+        for (j=0;j<cols-1;j++){
+            for (k=0;k<cols-1;k++){
                 if (*(*(arr+i)+k)>*(*(arr+i)+k+1)){
-                    
+                    temp= *(*(arr+i)+k+1);
+                    *(*(arr+i)+k+1)=*(*(arr+i)+k);
+                    *(*(arr+i)+k)= temp;
                 }
             }
         }
+    }
+    /*printing the matrix*/
+    printf("The elements are:\n");
+    for (i=0;i<rows;i++){
+        for (j=0;j<cols;j++){
+            printf("%d",*(*(arr+i)+j));
+            printf("\t");
+        }
+        printf("\n");
     }
     return 0;
 }
